64efc79749a4bc9bb839ca4c232722f714de63cf
cwanjau <cwanjau@microsoft.com>
2024-09-09 11:15:40 +0300

    update api

--- api/message/index.js
+++ api/message/index.js
@@ -1,8 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res.json = {
-        // status: 200, /* Defaults to 200 */
+    context.res.json({
         text: "Hello from the API"
-    };
-}
+    });
+};
